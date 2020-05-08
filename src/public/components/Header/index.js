ca90290a52b5c1465478f939b59fa5e9b2dc7721
Apurv Bazari <apurv.b@zolostays.com>
2020-05-08 16:38:05 +0530

    Key warning for header fixed

--- src/public/components/Header/index.js
+++ src/public/components/Header/index.js
@@ -5,7 +5,7 @@ import { Container, Title } from './style'
 const Header = (props) => {
   return(
     <Container>
-      {props.data.map((item, i) => <Title isLast={i === props.data.length - 1}>{item}</Title>)}
+      {props.data.map((item, i) => <Title key={i} isLast={i === props.data.length - 1}>{item}</Title>)}
     </Container>
   )
 }
